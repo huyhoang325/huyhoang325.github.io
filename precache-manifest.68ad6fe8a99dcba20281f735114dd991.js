8767af3b68703a2a4ed6d1d40f80d920e53a8e66
huyhoang325 <48050886+huyhoang325@users.noreply.github.com>
2019-07-03 09:19:33 +0700

    Add files via upload

---
+++ precache-manifest.68ad6fe8a99dcba20281f735114dd991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b2fc23be85f129334a4",
+    "url": "/static/js/main.2b2fc23b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "2b2fc23be85f129334a4",
+    "url": "/static/css/main.ae950d82.chunk.css"
+  },
+  {
+    "revision": "95c0968f3222cdaeaede57485582a695",
+    "url": "/index.html"
+  }
+];
